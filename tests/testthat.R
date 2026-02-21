6a269d45ab4a83b3750b4e36071dc2baad105415
Andrew Timm <timmandrew1@gmail.com>
2026-02-21 14:04:22 -0500

    1 more CRAN nit

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,12 @@
+# Limit BLAS/LAPACK threads to avoid CRAN "CPU time N times elapsed time" notes
+Sys.setenv(
+  OMP_NUM_THREADS = 1,
+  OPENBLAS_NUM_THREADS = 1,
+  MKL_NUM_THREADS = 1,
+  VECLIB_MAXIMUM_THREADS = 1,
+  BLAS_NUM_THREADS = 1
+)
+
 library(testthat)
 library(regrake)
 
